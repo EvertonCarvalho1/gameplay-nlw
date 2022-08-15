63ace92b8b6b355b457948bb14f1d8020373d3fc
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-08-15 09:19:34 -0300

    configuring splash screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput } from 'react-native';
 
 import { styles } from './styles';
 
 export function SignIn() {
+    const [text, setText] = useState('Everton');
+
 
     return (
         <View style={styles.container}>
             <Text>Hello World</Text>
+
+            <TextInput
+                style={styles.input}
+                onChangeText={(value) => setText(value)}
+            />
+
+            <Text>
+                Você digitou...{text}
+            </Text>
         </View>
     );
 }
