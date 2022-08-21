0711bc239630b6d94e8306278e7be242e5ada482
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-08-21 14:06:11 -0300

    configuring navigation

--- App.tsx
+++ App.tsx
@@ -4,10 +4,7 @@ import AppLoading from "expo-app-loading";
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
-
-import { SignIn } from './src/screens/SignIn';
-import { Home } from './src/screens/Home';
-import { Background } from "./src/components/Background";
+import { Routes } from "./src/routes";
 
 
 export default function App() {
@@ -23,13 +20,14 @@ export default function App() {
   }
 
   return (
-    <Background>
+    <>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-      <Home />
-    </Background>
+      <Routes />
+    </>
+
   );
 }  
