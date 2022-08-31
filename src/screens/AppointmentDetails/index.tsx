32a585b7267f113f1102d654328d5f5e9cf753e4
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-08-31 08:38:02 -0300

    appointment details card

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import { Fontisto } from '@expo/vector-icons';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+import {
+    ImageBackground,
+    Text
+} from 'react-native';
+
+import { theme } from "../../global/styles/theme";
+import BannerImg from '../../assets/banner.png';
 
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
@@ -10,7 +20,28 @@ export function AppointmentDetails() {
         <Background>
             <Header
                 title="Detalhes"
+                action={
+                    <BorderlessButton>
+                        <Fontisto
+                            name="share"
+                            size={24}
+                            color={theme.colors.primary}
+                        />
+                    </BorderlessButton>
+                }
             />
+
+            <ImageBackground
+                source={BannerImg}
+                style={styles.banner}
+            >
+                <Text style={styles.title}>
+                    Lendários
+                </Text>
+                <Text style={styles.subtitle}>
+                    É hoje que vamos chegar ao challenger sem perder uma partida da md10
+                </Text>
+            </ImageBackground>
         </Background>
     );
 } 
