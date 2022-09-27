567a187004049fee9cea7dcfffaced775cf79a96
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-09-27 12:18:00 -0300

    inserting the logout feature

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -32,6 +32,7 @@ type AuthContextData = {
     loading: boolean;
     user: User;
     signIn: () => Promise<void>;
+    signOut: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -85,6 +86,11 @@ function AuthProvider({ children }: AuthProviderProps) {
         }
     }
 
+    async function signOut() {
+        setUser({} as User);
+        await AsyncStorage.removeItem(COLLECTION_USERS);
+    }
+
     async function loadUserStorageData() {
         const storage = await AsyncStorage.getItem(COLLECTION_USERS);
 
@@ -101,7 +107,12 @@ function AuthProvider({ children }: AuthProviderProps) {
     }, []);
 
     return (
-        <AuthContext.Provider value={{ user, signIn, loading }}>
+        <AuthContext.Provider value={{
+            user,
+            signIn,
+            loading,
+            signOut
+        }}>
             {children}
         </AuthContext.Provider>
     )
